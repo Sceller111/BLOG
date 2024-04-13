33e9a10f96e86e3f46520d66067718bbe59c8939
Sceller111 <164723876+Sceller111@users.noreply.github.com>
2024-04-13 17:12:06 +0300

    first

---
+++ app/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+from app.models import User
+from wtforms.validators import ValidationError
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please check email.')
+class EditProfileForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=1, max=140)])
+    submit = SubmitField('Set status')
+
+
+
+
